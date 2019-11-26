c20c7312b32a090ecca8cbe7868776eb856961de
Eduardo Morales <eduardo.morales@metadrop.net>
2019-11-26 13:15:58 +0100

    Example transaction.

--- examples/get_simple_transaction.php
+++ examples/get_simple_transaction.php
@@ -13,9 +13,9 @@ $config = [
 ];
 $client = new Client($url, 'Introduce your merchant password', $config);
 
-$order = 'Introduce your order';
-$terminal = 'Introduce your terminal';
-$merchant_code = 'Introduce your merchant code';
+$order = 'Introduce Ds_Order';
+$terminal = 'Introduce Ds_terminal';
+$merchant_code = 'Introduce your Ds_merchantCode';
 $response = $client->getTransaction($order, $terminal, $merchant_code);
 
 print_r($response);
